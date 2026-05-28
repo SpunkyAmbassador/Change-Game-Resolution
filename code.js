e08a0da2306dd4159592df650c0658438c09842f
SpunkyAmbassador <spunkyambassador@proton.me>
2026-05-28 13:38:16 +0530

    Update CGR Plugin to work on RPM 3.x

--- code.js
+++ code.js
@@ -1,16 +1,11 @@
-import { RPM } from "../path.js"
-
 /**
  * This plugin allows to set the window resolution and full screen mode.
  */
 
 // Plugin name
-const pluginName = "Change Game Resolution";
+const pluginName = 'Change Game Resolution';
 
-// Require the file system module
-const fs = require('fs');
-
-const systemJSONFilePath = RPM.Common.Paths.FILE_SYSTEM;
+const systemJSONFilePath = Common.Paths.FILE_SYSTEM;
 
 /**
  * Set the window size to the specified width, height and full screen mode.
@@ -18,22 +13,20 @@ const systemJSONFilePath = RPM.Common.Paths.FILE_SYSTEM;
  * @param {number} height - The height of the window.
  * @param {boolean} fullscreen - Indicates if the window is in full screen mode.
  */
-function setWindowSize(width, height, fullscreen) {
-    // Read the JSON file
-    const jsonData = fs.readFileSync(systemJSONFilePath, 'utf8');
-    const data = JSON.parse(jsonData);
+async function setWindowSize(width, height, fullscreen) {
+    const data = await Common.IO.parseFileJSON(systemJSONFilePath);
 
-    // Modify the values
+    // Modify values
     data.sh = height;
     data.sw = width;
     data.isw = !fullscreen;
 
-    // Write the updated object back to the file
-    fs.writeFileSync(systemJSONFilePath, JSON.stringify(data, null, 2));
+    // Save object directly
+    await Common.Platform.writeFile(systemJSONFilePath, data);
 
     // Update the window size
-    RPM.Datas.Systems.updateWindowSize(width, height, fullscreen);
-    RPM.Datas.Systems.updateWindowSize(width, height, fullscreen);
+    Data.Systems.updateWindowSize(width, height, fullscreen);
+    Data.Systems.updateWindowSize(width, height, fullscreen);
 }
 
 /**
@@ -41,21 +34,21 @@ function setWindowSize(width, height, fullscreen) {
  * @param {number} Width - The width of the screen.
  * @param {number} Height - The height of the screen.
  */
-RPM.Manager.Plugins.registerCommand(pluginName, "Set Resolution", (Width, Height) => {
+Manager.Plugins.registerCommand(pluginName, 'Set Resolution', (Width, Height) => {
     setWindowSize(Width, Height, false);
 });
 
 /**
  * Set the game to full screen mode.
  */
-RPM.Manager.Plugins.registerCommand(pluginName, "Set Full Screen", () => {
-    setWindowSize(0, 0, true);
+Manager.Plugins.registerCommand(pluginName, 'Set Full Screen', () => {
+    setWindowSize(1280, 720, true);
 });
 
 /**
  * Confirm the resolution change.
  */
-RPM.Manager.Plugins.registerCommand(pluginName, "Reload Game", () => {
+Manager.Plugins.registerCommand(pluginName, 'Reload Game', () => {
     // Reload the window/game
     window.location.reload();
 });
